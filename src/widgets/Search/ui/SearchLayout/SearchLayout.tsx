cbc4a4e94e89798f09b34ba6fea2f2e82b64c821
Anton <popsof@yandex.ru>
2026-01-22 17:35:04 +0400

    feat: upd search

--- src/widgets/Search/ui/SearchLayout/SearchLayout.tsx
+++ src/widgets/Search/ui/SearchLayout/SearchLayout.tsx
@@ -1,11 +1,15 @@
+import { Input } from "@/shared";
 import React, { FC, PropsWithChildren, useState, useEffect } from "react";
 
 export const SearchLayout: FC<PropsWithChildren> = ({ children }) => {
-  const [] = useState<number[]>([]);
+  // const [] = useState<number[]>([]);
+  const [layoutFocus, setLayoutFocus] = useState<boolean>(false);
 
   return (
-    <div className="flex justify-center rounded-2xl bg-[#8686f3b2] h-full w-4/5">
-      <div className="flex items-center w-[95%]">{children}</div>
+    <div className="flex justify-center border-[#8686f3cb] rounded-2xl bg-[#8686f3b2] h-full w-4/5 focus-within:border-blue-[]">
+      <div className="flex items-center w-[95%]">
+        <input className="w-full h-full outline-none" type="text" />
+      </div>
     </div>
   );
 };
