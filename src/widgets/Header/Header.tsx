f1cfbae1738557d91349e34f1bfcb2a10cfba720
Anton <popsof@yandex.ru>
2026-01-22 17:14:08 +0300

    feat: add cart link

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -5,13 +5,17 @@ import Image from "next/image";
 import { Search } from "../Search";
 import { Button } from "@/shared";
 import { useModal } from "@/shared";
-import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { ButtonVariant, ButtonType } from "@/shared/ui/Button";
 import { CommonSize } from "@/types";
 
 export const Header = () => {
   const openModal = useModal((state) => state.openModalWindow);
+
+  const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <header className="relative flex justify-center h-[12vh] bg-custom-gradient after:content-[''] after:block after:w-full after:h-6 after:rounded-t-[24px] after:bg-white after:absolute after:-bottom-px after:left-0">
@@ -29,13 +33,17 @@ export const Header = () => {
             <div className="flex items-center"></div>
           </div>
           <Search />
-          <Button
-            variant={ButtonVariant.bordered}
-            type={ButtonType.button}
-            size={CommonSize.sm}
-          >
-            <Link href={"/"}>корзина</Link>
-          </Button>
+          <div className="flex items-center gap-sm w-fit">
+            <Button
+              variant={ButtonVariant.bordered}
+              type={ButtonType.button}
+              size={CommonSize.sm}
+              onClick={() => router.push("/cart")}
+              className={pathname === "/cart" ? "border-0 border-white" : ""}
+            >
+              <Image src={"/cart.svg"} height={32} width={32} alt="" />
+            </Button>
+          </div>
         </div>
       </div>
     </header>
