8593f9b7442c069cc745314596fe1bad3284ea8f
Anton <popsof@yandex.ru>
2026-01-22 17:17:34 +0300

    feat: upd base layout

--- src/app/(base-layout)/cart/Cart.tsx
+++ src/app/(base-layout)/cart/Cart.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Button } from "@/shared";
+import { CommonSize } from "@/types";
+import { ButtonType } from "@/shared/ui/Button";
+import { ButtonVariant } from "@/shared/ui/Button";
 
 const Cart = () => {
-  return <div className="">hello world</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <Button
+        size={CommonSize.sm}
+        variant={ButtonVariant.bordered}
+        type={ButtonType.button}
+        className="border-[#243c5a]"
+      >
+        hello
+      </Button>
+      <Button
+        size={CommonSize.sm}
+        variant={ButtonVariant.bordered}
+        type={ButtonType.button}
+      >
+        world
+      </Button>
+    </div>
+  );
 };
 
 export default Cart;
