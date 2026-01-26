5d8780d72c9a2cf883c9526fdf82db4bccced45b
Anton <popsof@yandex.ru>
2026-01-26 23:46:50 +0400

    feat: add scroll lock

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -1,13 +1,27 @@
 "use client";
 
-import React, { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren, useEffect } from "react";
 import { Header } from "@/widgets";
 import { Footer } from "@/widgets";
 import { ModalLayout } from "@/shared";
 import { useModal } from "@/shared";
 
+
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   const { isOpen } = useModal();
+
+  useEffect(()=>{ 
+    if(isOpen){ 
+      document.body.style.overflow = "hidden";
+    }else{ 
+      document.body.style.overflow = "";
+    }
+
+    return ()=>{ 
+      document.body.style.overflow = "";
+    }
+  },[isOpen])
+
 
   return (
     <div className="flex min-h-screen flex-col">
