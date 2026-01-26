9c550c13c5af8845c7ed402378a92198b1324c1f
Anton <popsof@yandex.ru>
2026-01-26 23:25:04 +0400

    style: apply formatting

--- src/shared/ui/Modals/store/ModalStore.ts
+++ src/shared/ui/Modals/store/ModalStore.ts
@@ -1,19 +1,5 @@
 import { create } from "zustand";
 
-// interface BearState {
-//   bears: number;
-//   increasePopulation: () => void;
-//   removeAllBears: () => void;
-//   updateBears: (newBears: number) => void;
-// }
-
-// const useBear = create<BearState>((set) => ({
-//   bears: 0,
-//   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-//   removeAllBears: () => set({ bears: 0 }),
-//   updateBears: (newBears) => set({ bears: newBears }),
-// }))
-
 type CurrentModal = "SideMenuModal" | null;
 
 interface ModalState {
