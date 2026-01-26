29730964aa75682c14737467e88a38a8a90cdea8
Anton <popsof@yandex.ru>
2026-01-26 23:26:00 +0400

    refactor(footer): rework layout

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,65 +1,20 @@
 "use client";
 
 import React from "react";
-import styles from "./footer.module.css";
+import { footerSection } from "./footer.model";
+import { FooterSection } from "./FooterSection";
 
 export const Footer = () => {
   return (
-    <footer className="flex bg-custom-gradient">
-      <div className={styles["conteiner__footer"]}>
-        <section className={styles["content__1"]}>
-          <div className={styles["footer__header-wrap"]}>
-            <h2 className={styles["content__title"]}>Покупателям</h2>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Как совершdить покупку</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a href="../page/dostavka.php">Достака</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Есть ли рассрочка</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Возврат</a>
-          </div>
-        </section>
-
-        <section className={styles["content__1"]}>
-          <div className={styles["footer__header-wrap"]}>
-            <h2 className={styles["content__title"]}>Компания</h2>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>О нас</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Где мы офлайн</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Контакты</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Реквезиты</a>
-          </div>
-        </section>
-
-        <section className={styles["content__1"]}>
-          <div className={styles["footer__header-wrap"]}>
-            <h2 className={styles["content__title"]}>Наши соц.сети</h2>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Telegram</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Группа ВКонтакте</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>YouTube</a>
-          </div>
-          <div className={styles["content__title"]}>
-            <a>Instagram</a>
-          </div>
-        </section>
+    <footer className="relative flex justify-center bg-custom-gradient align-baseline before:content-[''] before:block before:w-full before:h-6 before:rounded-b-[24px] before:bg-white before:absolute before:top-0 before:left-0 pt-8">
+      <div className="flex items-center justify-center gap-md w-[70%] h-fit m-[6px] text-white">
+        {footerSection.map((item) => (
+          <FooterSection
+            key={item.title}
+            title={item.title}
+            link={item.links}
+          />
+        ))}
       </div>
     </footer>
   );
