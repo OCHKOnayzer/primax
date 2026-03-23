7c978e32684fc031f57be7a04a1873ddbb524da5
Anton <popsof@yandex.ru>
2026-03-23 19:00:28 +0400

    feat: add app provider

--- src/shared/providers/AppProviders.tsx
+++ src/shared/providers/AppProviders.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import React, {useEffect, useState} from 'react';
-import { initMocks } from '@/mock/initMocks';
-import { Loader } from '../ui/Loader';
+import React, { useEffect, useState } from "react";
+import { initMocks } from "@/mock/initMocks";
+import { Loader } from "../ui/Loader";
 
-export const AppProviders = ({ children }: {children: React.ReactNode}) => {
- const [ready, setReady] = useState(false);
+export const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
     async function start() {
@@ -16,7 +16,7 @@ export const AppProviders = ({ children }: {children: React.ReactNode}) => {
     start();
   }, []);
 
-  if (!ready) return <Loader/>;
+  if (!ready) return <Loader />;
 
   return <>{children}</>;
-}
+};
