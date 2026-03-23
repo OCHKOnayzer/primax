4c4195a8f188590eb70966ab4693933593ca02ea
Anton <popsof@yandex.ru>
2026-03-23 19:07:19 +0400

    feat: upd export file

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,7 @@
-export { ModalLayout } from "./ui/Modals";
-export { useModal } from "./ui/Modals";
-export { Button } from "./ui/Button";
+export { Loader } from "./ui/Loader";
+export { AppProviders } from "./providers";
+export { CurrentModalEnum } from "./ui/Modals";
+export { ModalLayout, useModal } from "./ui/Modals";
+export { Button, ButtonVariant, ButtonType } from "./ui/Button";
 export { Layout } from "./ui/Layout";
 export { Input } from "./ui/Input";
