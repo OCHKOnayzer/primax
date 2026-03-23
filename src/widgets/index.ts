ae5ee32916b7c87b418432e2c3547b42b0699e27
Anton <popsof@yandex.ru>
2026-03-23 19:05:55 +0400

    feat: upd export file

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,3 +1,5 @@
+export { Slider } from "./Slider";
+export { CardLayout } from "./CardLayout";
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { Search } from "./Search";
