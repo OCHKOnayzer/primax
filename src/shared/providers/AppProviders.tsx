aff1804cd30777659a8ca901bce3a81b46ce700a
Anton <popsof@yandex.ru>
2026-04-13 15:16:20 +0400

    feat: add tanstack logic

--- src/shared/providers/AppProviders.tsx
+++ src/shared/providers/AppProviders.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { QueryClientProvider } from '@tanstack/react-query'
 import { initMocks } from "@/mock/initMocks";
+import { createQueryClient } from "./queryProvider";
 import { Loader } from "../ui/Loader";
 
 export const AppProviders = ({ children }: { children: React.ReactNode }) => {
   const [ready, setReady] = useState(false);
+  const [queryClient] = useState(() => createQueryClient())
 
   useEffect(() => {
     async function start() {
@@ -18,5 +21,5 @@ export const AppProviders = ({ children }: { children: React.ReactNode }) => {
 
   if (!ready) return <Loader />;
 
-  return <>{children}</>;
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
